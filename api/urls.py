dff586a7568b8b2ebb0582fd25d5d401cc313348
yerke <zat.tyme.su@gmail.com>
2023-04-19 03:41:27 +0600

    Modified urls

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
-from api.views import (companies_list, company_detail,
-                       company_vacancies, vacancy_list,
-                       vacancy_detail, vacancy_top_ten)
+from rest_framework.routers import DefaultRouter
+from api.views import CompanyViewSet, \
+    vacancies_view, vacancy_detail_view, vacancy_top_ten
 
-urlpatterns = [
-    path('companies/', companies_list),
-    path('companies/<int:pk>', company_detail),
-    path('companies/<int:pk>/vacancies', company_vacancies),
-    path('vacancies/', vacancy_list),
-    path('vacancies/<int:pk>', vacancy_detail),
+
+company_router = DefaultRouter()
+company_router.register(r'companies', CompanyViewSet, basename='company')
+
+# vacancy_router = DefaultRouter()
+# vacancy_router.register(r'vacancies', VacancyViewSet, basename='vacancy')
+
+urlpatterns = company_router.urls
+# urlpatterns += vacancy_router.urls
+
+urlpatterns += [
+    # path('companies/', CompanyViewSet.as_view({'get': 'list'})),
+    # path('companies/<int:pk>', CompanyViewSet.as_view({'get': 'retrieve'})),
+    # path('companies/<int:pk>/vacancies', company_vacancies),
+    path('vacancies/', vacancies_view),
+    path('vacancies/<int:pk>', vacancy_detail_view),
     path('vacancies/top_ten/', vacancy_top_ten),
 ]
