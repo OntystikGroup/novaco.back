ddbfcf7093effdf997efca518c56b95886194a74
yerke <zat.tyme.su@gmail.com>
2023-05-03 11:25:43 +0600

    Last commit: permissions

---
+++ api/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
+
+
+class IsAuthenticatedStaffOrReadOnly(IsAuthenticatedOrReadOnly):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_staff
+        )
